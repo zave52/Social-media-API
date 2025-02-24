4c3e30add6d79cfdb2773b1414046d3c65528897
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-24 23:34:27 +0200

    feat: create ManageUserView and CreateUserView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = ()
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self) -> get_user_model():
+        return self.request.user
