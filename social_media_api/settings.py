0b4b5ff1bb2f3c3fe3b4d626ed7d4fad1bc82ee3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-24 23:53:32 +0200

    feat: install dotenv and configure load environment variables

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x@7j_c$ybx51ew&m#ij^9k+!7od^$xrb6a_i#cyhxj#$b9k4o1"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG").lower() in ("true", "t", "1")
 
 ALLOWED_HOSTS = []
 
