4dda2e73b4e407371f54b2148ea7d6ed96346194
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-25 08:52:35 +0200

    feat: create Post model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import pathlib
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.text import slugify
+
+
+def post_image_upload(instance: "Post", filename: str) -> pathlib.Path:
+    filename = (
+        f"{slugify(instance.title)}-{uuid.uuid4}"
+        + pathlib.Path(filename).suffix
+    )
+    return pathlib.Path("upload/posts/") / pathlib.Path(filename)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=post_image_upload
+    )
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
