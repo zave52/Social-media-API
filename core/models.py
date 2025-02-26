cdf86b7c35cffc60f91f7862d3a42f3623cf790a
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-26 22:38:27 +0200

    refactor: change tags to ManyToManyField

--- core/models.py
+++ core/models.py
@@ -47,11 +47,7 @@ class Post(models.Model):
         related_name="posts"
     )
     created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ForeignKey(
-        Tag,
-        on_delete=models.CASCADE,
-        related_name="posts"
-    )
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
 
     def __str__(self) -> str:
         return self.title
