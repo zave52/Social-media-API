f15505f69137bf56947207dbb1fef78ed73f4977
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-27 09:05:51 +0200

    feat: create base of ProfileViewSet

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from rest_framework import generics, viewsets, mixins, serializers
 
-# Create your views here.
+from core.models import Profile
+from core.serializers import ProfileSerializer, ProfileListSerializer
+
+
+class ProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_serializer_class(self) -> type(serializers.ModelSerializer):
+        if self.action == "list":
+            return ProfileListSerializer
+        return self.serializer_class
