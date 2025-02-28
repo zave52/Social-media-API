fd6cc0239c2088c509e3efaa19a5fec82b15d4d7
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-02-28 18:39:01 +0200

    feat: add drf-spectacular paths in urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -17,9 +17,28 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("social-media/", include("core.urls", namespace="social_media")),
-    path("user/", include("user.urls", namespace="user")),
+    path(
+        "api/v1/social-media/",
+        include("core.urls", namespace="social_media")
+    ),
+    path("api/v1/user/", include("user.urls", namespace="user")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
 ]
