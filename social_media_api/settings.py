4cbf130544cd663272657cd65fad64fb194deb80
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-03 21:46:29 +0200

    feat: configure whitenoise use

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/vol/static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "/vol/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
