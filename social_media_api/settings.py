eea2400dd693bde07734fe21da3e576b285d037f
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-03 22:18:20 +0200

    feat: add 0.0.0.0 to allowed hosts

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG").lower() in ("true", "t", "1")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 # Application definition
 
